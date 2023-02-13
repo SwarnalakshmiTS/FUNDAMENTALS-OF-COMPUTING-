492de53965c653bfcbd558cd39f11cb2f51e6e8f
SwarnalakshmiTS <112744358+SwarnalakshmiTS@users.noreply.github.com>
2023-02-13 14:55:41 +0530

    Create quadratic equation.sh

---
+++ quadratic equation.sh
@@ -0,0 +1,9 @@
+echo "enter the value a"
+read a
+echo "enter the value b"
+read b
+echo "enter the value c"
+read c
+d=`expr $b \* $b -4\* $a \* $c`
+x1=`echo "scale=3;(-$b + sqrt($d)) / (2 * $a)"| be `x2=`echo "scale=3;(-$b - sqrt($d)) / (2 * $a)"| be`echo "root 1 : $x1"
+echo "root 2 : $x2"
